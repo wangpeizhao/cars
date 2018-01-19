5dfdd0d209b56121104d7c6108845a1cc8620e01
wangpeizhao <342823274@qq.com>
2018-01-20 00:44:24 +0800

    home module

--- application/views/default/home.php
+++ application/views/default/home.php
@@ -72,9 +72,53 @@
                             }?>
                         </ul>
                     </div>
+                    <div class="hotFocus">
+                        <ul>
+                            <li>
+                                <a href="http://36kr.com/p/5114445.html"><img src="<?=img_url('u33ugxahv1vfjegl!heading.jpeg')?>"></a>
+                                <a href="http://36kr.com/p/5114445.html"><span>区块链+SaaS系统服务餐饮企业，「亿点点」获A轮融资</span></a>
+                            </li><li>
+                                <a href="http://36kr.com/p/5114526.html"><img src="<?=img_url('rgpz4dt5crigsxwu!heading.jpeg')?>"></a>
+                                <a href="http://36kr.com/p/5114526.html"><span>SpaceCycle完成B轮融资，阿里台湾创业者基金领投</span></a>
+                            </li><li>
+                                <a href="http://36kr.com/p/5112135.html"><img src="<?=img_url('n1cuq3gm3a0rgkqz!heading.jpeg')?>"></a>
+                                <a href="http://36kr.com/p/5112135.html"><span>「分子未来」要做区块链领域的“天天基金网”+“网贷之家”</span></a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="list_con">
+                        <div class="car_tab">
+                            <ul>
+                                <li class="active"><span><a href="">最新文章</a></span></li>
+                                <li><span><a href="">早期项目</a></span></li>
+                                <li><span><a href="">大公司</a></span></li>
+                                <li><span><a href="">创投新闻</a></span></li>
+                                <li><span><a href="">AI is</a></span></li>
+                                <li><span><a href="">消费升级</a></span></li>
+                                <li><span><a href="">深氪</a></span></li>
+                                <li><span><a href="">技能Get</a></span></li>
+                            </ul>
+                        </div>
+                        <div class="car_article_list">
+                            <ul>
+                                <li><div class="am-cf inner_li inner_li_abtest"><a data-stat-click="103.zhufeed.wenzhangbeijing.1" href="/p/5114673.html" target="_blank"><div class="img_box"><div data-stat-click="103.zhufeed.wenzhangtupian.1" href="/p/5114673.html" target="_blank"><img src="https://pic.36krcnd.com/201801/18090922/yh6d36x9nxl3ulxb!heading" alt="微信是怎么做到亿级用户的异常检测的？" class="load-img fade"></div></div><div class="intro"><h3 data-stat-click="103.zhufeed.wenzhangbiaoti.1">微信是怎么做到亿级用户的异常检测的？</h3><div class="abstract">本文将带你一窥究竟，微信是怎么做异常检测框架的？</div></div></a><div class="info"><div class="info-list info-list-abtest"><div class="user-info"><a href="/user/323686438" data-stat-click="zhufeed.wenzhangzuozhe.1.5114673" target="_blank" class="name">InfoQ技术媒体</a><span class="oblique_line">·</span></div><div class="time-div"><span class="time" title="2018-01-18 21:07">3小时前</span><span class="time h5_time">3小时前</span></div></div><div class="tags-list"><i class="icon-tag"></i><span><a href="/tag/%E5%A4%A7%E5%85%AC%E5%8F%B8" target="_blank">大公司</a><span>，</span></span><span><a href="/tag/%E4%BC%81%E4%B8%9A%E6%9C%8D%E5%8A%A1" target="_blank">企业服务</a></span></div><div class="comments-list"><span class="comments">10收藏</span></div></div></div></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div><!-- $main --><!-- ^right side --><div class="rightlib">
                     <div class="ads ">
                         <a href="#" style="background-image: url('<?=img_url('un68ifk27tary2fy.jpeg')?>')"></a>
+                        <span class="mark">广告</span>
+                    </div>
+
+                    <div class="real_time">
+                        <h3><span>7×24h 快讯</span></h3>
+                        <ul>
+                            <li class="real_time_wrapper">
+                                
+                            </li>
+                        </ul>
                     </div>
                 </div>
                 <!-- $right side -->
